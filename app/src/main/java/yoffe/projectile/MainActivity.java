31f641df9a3ac95d8cc2112cca5601c5bb80c95d
unknown <18saray@gmail.com>
2015-09-21 22:32:20 -0400

    Added Projectile App

--- app/src/main/java/yoffe/projectile/MainActivity.java
+++ app/src/main/java/yoffe/projectile/MainActivity.java
@@ -4,13 +4,51 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView angle;
+    private EditText angleInput;
+    private TextView velocity;
+    private EditText velocityInput;
+    private TextView time;
+    private EditText timeInput;
+    private Button projectile;
+    private TextView answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        angle = (TextView)findViewById(R.id.angle);
+        angleInput = (EditText)findViewById(R.id.angleInput);
+        velocity = (TextView)findViewById(R.id.velocity);
+        velocityInput = (EditText)findViewById(R.id.velocityInput);
+        time = (TextView)findViewById(R.id.time);
+        timeInput = (EditText)findViewById(R.id.timeInput);
+        projectile = (Button)findViewById(R.id.projectile);
+        answer = (TextView)findViewById(R.id.answer);
+
+        projectile.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                double radians = Math.toRadians(Double.parseDouble(angleInput.getText().toString()));
+                double velocity = Double.parseDouble(velocityInput.getText().toString());
+                double time=Double.parseDouble(timeInput.getText().toString());
+                double x =getX(radians, velocity, time);
+                double y=getY(radians, velocity, time);
+
+                StringBuilder builder = new StringBuilder();
+                builder.append(x);
+                builder.append(", ");
+                builder.append(y);
+                answer.setText(builder.toString());
+            }
+        });
     }
 
     @Override
@@ -33,5 +71,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private double getY(double radians, double velocity, double time) {
+        return Math.cos(radians) * velocity * time - (1 / 2 * 9.8 * time * time);
+    }
+
+
+    private double getX(double radians, double velocity, double time) {
+        return Math.sin(radians) * velocity * time;
     }
 }
